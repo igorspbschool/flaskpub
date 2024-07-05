4a9a7e0bb3ee9d12af6d186c5f2c71653f4f5470
igorspbschool <igorspbschool@yandex.ru>
2024-07-05 14:08:58 +0300

    com1

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 from flask import Flask
-#библиотека для передачи запроса чере WSGI сервер
 from waitress import serve
 
 app = Flask(__name__)
@@ -12,5 +11,4 @@ def index():
 
 
 if __name__ == '__main__':
-    # app.run(port=8080, host='0.0.0.0')
     serve(app, host='0.0.0.0', port=8080)
